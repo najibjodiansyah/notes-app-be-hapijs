12b6bbc75afaedd88bde7fac5d6a9d8ef3fef938
najibjodiansyah <najibjodiansyah11@gmail.com>
2022-04-06 01:31:20 +0700

    init http server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log('Hallo kita akan membuat REST api');
+const Hapi = require('@hapi/hapi');
+
+const Init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+Init();
